277b16dd7c4ce7b3877fc5592a119b72e7fd3a06
the-boys <ln08101996@gmail.com>
2024-04-02 12:45:25 +0700

    gialuan commit3

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -34,6 +34,7 @@
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
               with font-awesome or any other icon font library -->
+              @if (Auth::user()->level==1)
           <li class="nav-item">
               <a href="#" class="nav-link">
                   <i class="nav-icon fas fa-sitemap"></i>
@@ -128,6 +129,9 @@
                 </li>
             </ul>
         </li>
+       
+            
+        @endif
         <li class="nav-item">
             <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-users"></i>
